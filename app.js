d95db7f9a08633266fdcd91c0224f23e3618e682
hasanabdullah109 <64689886+hasanabdullah109@users.noreply.github.com>
2020-06-14 17:08:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Assignment# 1
+
+// Q# 1
+alert("welcome to our page. Thanks for visit.")
+
+// Q# 2
+
+alert("Error! Please enter a valid password")
+
+// Q# 3
+
+alert("Welcome To JS Land..... \n Happy Coding! ");
+
+// Q# 4
+
+alert("Welcometo JS Land")
+
